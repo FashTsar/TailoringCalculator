be431d6958128aa971c91eaac3eccedc69eaf050
FashTsar <fash-rus37@mail.ru>
2018-04-18 16:01:59 +0300

    добавил проверку, если менее 0,01 м, то округляется до 0,01 м

--- src/com/company/ResultPage.java
+++ src/com/company/ResultPage.java
@@ -62,10 +62,15 @@ public class ResultPage extends JFrame {
         int resultPillowcaseType1 = PillowcaseType1.CalculationPillowcase(PillowcaseData[0][0], PillowcaseData[1][0],
                 PillowcaseData[3][0], PillowcaseData[2][0], PillowcaseData[4][0], WidthCloth, HandSelectionPillowcase[0]);
 
+        float fresultPillowcaseType1 = resultPillowcaseType1/100.00f;
+        if (fresultPillowcaseType1 < 0.01f && PillowcaseData[2][0] != 0){
+            fresultPillowcaseType1 = 0.01f;
+        }
+
         JLabel titlePillowcaseType1Lable = new JLabel("Размер наволочек: "+
                 PillowcaseData[0][0]+" x "+PillowcaseData[1][0]+" Количество: "+PillowcaseData[2][0]
                 +" шт. Необходимо = "
-                +f.format(resultPillowcaseType1/100.00)+" метров");
+                +f.format(fresultPillowcaseType1)+" метров");
         titlePillowcaseType1Lable.setVisible(false);
 
         JLabel exeptionPillowcaseType1Lable = new JLabel("Наволочки "+
@@ -94,10 +99,15 @@ public class ResultPage extends JFrame {
         int resultPillowcaseType2 = PillowcaseType2.CalculationPillowcase(PillowcaseData[0][1], PillowcaseData[1][1],
                 PillowcaseData[3][1], PillowcaseData[2][1], PillowcaseData[4][1], WidthCloth, HandSelectionPillowcase[1]);
 
+        float fresultPillowcaseType2 = resultPillowcaseType2/100.00f;
+        if (fresultPillowcaseType2 < 0.01f && PillowcaseData[2][1] != 0){
+            fresultPillowcaseType2 = 0.01f;
+        }
+
         JLabel titlePillowcaseType2Lable = new JLabel("Размер наволочек: "+
                 PillowcaseData[0][1]+" x "+PillowcaseData[1][1]+" Количество: "+PillowcaseData[2][1]
                 +" шт. Необходимо = "
-                +f.format(resultPillowcaseType2/100.00)+" метров");
+                +f.format(fresultPillowcaseType2)+" метров");
         titlePillowcaseType2Lable.setVisible(false);
 
         JLabel exeptionPillowcaseType2Lable = new JLabel("Наволочки "+
@@ -126,10 +136,15 @@ public class ResultPage extends JFrame {
         int resultPillowcaseType3 = PillowcaseType3.CalculationPillowcase(PillowcaseData[0][2], PillowcaseData[1][2],
                 PillowcaseData[3][2], PillowcaseData[2][2], PillowcaseData[4][2], WidthCloth, HandSelectionPillowcase[2]);
 
+        float fresultPillowcaseType3 = resultPillowcaseType3/100.00f;
+        if (fresultPillowcaseType3 < 0.01f && PillowcaseData[2][2] != 0){
+            fresultPillowcaseType3 = 0.01f;
+        }
+
         JLabel titlePillowcaseType3Lable = new JLabel("Размер наволочек: "+
                 PillowcaseData[0][2]+" x "+PillowcaseData[1][2]+" Количество: "+PillowcaseData[2][2]
                 +" шт. Необходимо = "
-                +f.format(resultPillowcaseType3/100.00)+" метров");
+                +f.format(fresultPillowcaseType3)+" метров");
         titlePillowcaseType3Lable.setVisible(false);
 
         JLabel exeptionPillowcaseType3Lable = new JLabel("Наволочки "+
@@ -158,10 +173,15 @@ public class ResultPage extends JFrame {
         int resultPillowcaseType4 = PillowcaseType4.CalculationPillowcase(PillowcaseData[0][3], PillowcaseData[1][3],
                 PillowcaseData[3][3], PillowcaseData[2][3], PillowcaseData[4][3], WidthCloth, HandSelectionPillowcase[3]);
 
+        float fresultPillowcaseType4 = resultPillowcaseType4/100.00f;
+        if (fresultPillowcaseType4 < 0.01f && PillowcaseData[2][3] != 0){
+            fresultPillowcaseType4 = 0.01f;
+        }
+
         JLabel titlePillowcaseType4Lable = new JLabel("Размер наволочек: "+
                 PillowcaseData[0][3]+" x "+PillowcaseData[1][3]+" Количество: "+PillowcaseData[2][3]
                 +" шт. Необходимо = "
-                +f.format(resultPillowcaseType4/100.00)+" метров");
+                +f.format(fresultPillowcaseType4)+" метров");
         titlePillowcaseType4Lable.setVisible(false);
 
         JLabel exeptionPillowcaseType4Lable = new JLabel("Наволочки "+
@@ -190,10 +210,15 @@ public class ResultPage extends JFrame {
         int resultPillowcaseType5 = PillowcaseType5.CalculationPillowcase(PillowcaseData[0][4], PillowcaseData[1][4],
                 PillowcaseData[3][4], PillowcaseData[2][4], PillowcaseData[4][4], WidthCloth, HandSelectionPillowcase[4]);
 
+        float fresultPillowcaseType5 = resultPillowcaseType5/100.00f;
+        if (fresultPillowcaseType5 < 0.01f && PillowcaseData[2][4] != 0){
+            fresultPillowcaseType5 = 0.01f;
+        }
+
         JLabel titlePillowcaseType5Lable = new JLabel("Размер наволочек: "+
                 PillowcaseData[0][4]+" x "+PillowcaseData[1][4]+" Количество: "+PillowcaseData[2][4]
                 +" шт. Необходимо = "
-                +f.format(resultPillowcaseType5/100.00)+" метров");
+                +f.format(fresultPillowcaseType5)+" метров");
         titlePillowcaseType5Lable.setVisible(false);
 
         JLabel exeptionPillowcaseType5Lable = new JLabel("Наволочки "+
@@ -222,10 +247,15 @@ public class ResultPage extends JFrame {
         int resultQuiltType1 = QuiltType1.CalculationQuilt(QuiltData[0][0], QuiltData[1][0],
                 QuiltData[2][0], QuiltData[3][0], WidthCloth, HandSelectionQuilt[0]);
 
+        float fresultQuiltType1 = resultQuiltType1/100.00f;
+        if (fresultQuiltType1 < 0.01f && QuiltData[2][0] != 0){
+            fresultQuiltType1 = 0.01f;
+        }
+
         JLabel titleresultQuiltType1Lable = new JLabel("Размер пододеяльника: "+
                 QuiltData[0][0]+" x "+QuiltData[1][0]+" Количество: "+QuiltData[2][0]
                 +" шт. Необходимо = "
-                +f.format(resultQuiltType1/100.00)+" метров");
+                +f.format(fresultQuiltType1)+" метров");
         titleresultQuiltType1Lable.setVisible(false);
 
         JLabel exeptionQuiltType1Lable = new JLabel("Пододеяльники "+
@@ -254,10 +284,15 @@ public class ResultPage extends JFrame {
         int resultQuiltType2 = QuiltType2.CalculationQuilt(QuiltData[0][1], QuiltData[1][1],
                 QuiltData[2][1], QuiltData[3][1], WidthCloth, HandSelectionQuilt[1]);
 
+        float fresultQuiltType2 = resultQuiltType2/100.00f;
+        if (fresultQuiltType2 < 0.01f && QuiltData[2][1] != 0){
+            fresultQuiltType2 = 0.01f;
+        }
+
         JLabel titleresultQuiltType2Lable = new JLabel("Размер пододеяльника: "+
                 QuiltData[0][1]+" x "+QuiltData[1][1]+" Количество: "+QuiltData[2][1]
                 +" шт. Необходимо = "
-                +f.format(resultQuiltType2/100.00)+" метров");
+                +f.format(fresultQuiltType2)+" метров");
         titleresultQuiltType2Lable.setVisible(false);
 
         JLabel exeptionQuiltType2Lable = new JLabel("Пододеяльники "+
@@ -286,10 +321,15 @@ public class ResultPage extends JFrame {
         int resultQuiltType3 = QuiltType3.CalculationQuilt(QuiltData[0][2], QuiltData[1][2],
                 QuiltData[2][2], QuiltData[3][2], WidthCloth, HandSelectionQuilt[2]);
 
+        float fresultQuiltType3 = resultQuiltType3/100.00f;
+        if (fresultQuiltType3 < 0.01f && QuiltData[2][2] != 0){
+            fresultQuiltType3 = 0.01f;
+        }
+
         JLabel titleresultQuiltType3Lable = new JLabel("Размер пододеяльника: "+
                 QuiltData[0][2]+" x "+QuiltData[1][2]+" Количество: "+QuiltData[2][2]
                 +" шт. Необходимо = "
-                +f.format(resultQuiltType3/100.00)+" метров");
+                +f.format(fresultQuiltType3)+" метров");
         titleresultQuiltType3Lable.setVisible(false);
 
         JLabel exeptionQuiltType3Lable = new JLabel("Пододеяльники "+
@@ -318,10 +358,15 @@ public class ResultPage extends JFrame {
         int resultQuiltType4 = QuiltType4.CalculationQuilt(QuiltData[0][3], QuiltData[1][3],
                 QuiltData[2][3], QuiltData[3][3], WidthCloth, HandSelectionQuilt[3]);
 
+        float fresultQuiltType4 = resultQuiltType4/100.00f;
+        if (fresultQuiltType4 < 0.01f && QuiltData[2][3] != 0){
+            fresultQuiltType4 = 0.01f;
+        }
+
         JLabel titleresultQuiltType4Lable = new JLabel("Размер пододеяльника: "+
                 QuiltData[0][3]+" x "+QuiltData[1][3]+" Количество: "+QuiltData[2][3]
                 +" шт. Необходимо = "
-                +f.format(resultQuiltType4/100.00)+" метров");
+                +f.format(fresultQuiltType4)+" метров");
         titleresultQuiltType4Lable.setVisible(false);
 
         JLabel exeptionQuiltType4Lable = new JLabel("Пододеяльники "+
@@ -350,10 +395,15 @@ public class ResultPage extends JFrame {
         int resultQuiltType5 = QuiltType5.CalculationQuilt(QuiltData[0][4], QuiltData[1][4],
                 QuiltData[2][4], QuiltData[3][4], WidthCloth, HandSelectionQuilt[4]);
 
+        float fresultQuiltType5 = resultQuiltType5/100.00f;
+        if (fresultQuiltType5 < 0.01f && QuiltData[2][4] != 0){
+            fresultQuiltType5 = 0.01f;
+        }
+
         JLabel titleresultQuiltType5Lable = new JLabel("Размер пододеяльника: "+
                 QuiltData[0][4]+" x "+QuiltData[1][4]+" Количество: "+QuiltData[2][4]
                 +" шт. Необходимо = "
-                +f.format(resultQuiltType5/100.00)+" метров");
+                +f.format(fresultQuiltType5)+" метров");
         titleresultQuiltType5Lable.setVisible(false);
 
         JLabel exeptionQuiltType5Lable = new JLabel("Пододеяльники "+
@@ -382,10 +432,15 @@ public class ResultPage extends JFrame {
         int resultSheetType1 = SheetType1.CalculationSheet(SheetData[0][0], SheetData[1][0],
                 SheetData[2][0], SheetData[3][0], WidthCloth, HandSelectionSheet[0]);
 
+        float fresultSheetType1 = resultSheetType1/100.00f;
+        if (fresultSheetType1 < 0.01f && SheetData[2][0] != 0){
+            fresultSheetType1 = 0.01f;
+        }
+
         JLabel titleresultSheetType1Lable = new JLabel("Размер простыни: "+
                 SheetData[0][0]+" x "+SheetData[1][0]+" Количество: "+SheetData[2][0]
                 +" шт. Необходимо = "
-                +f.format(resultSheetType1/100.00)+" метров");
+                +f.format(fresultSheetType1)+" метров");
         titleresultSheetType1Lable.setVisible(false);
 
         JLabel exeptionSheetType1Lable = new JLabel("Простыни "+
@@ -414,10 +469,15 @@ public class ResultPage extends JFrame {
         int resultSheetType2 = SheetType2.CalculationSheet(SheetData[0][1], SheetData[1][1],
                 SheetData[2][1], SheetData[3][1], WidthCloth, HandSelectionSheet[1]);
 
+        float fresultSheetType2 = resultSheetType2/100.00f;
+        if (fresultSheetType2 < 0.01f && SheetData[2][1] != 0){
+            fresultSheetType2 = 0.01f;
+        }
+
         JLabel titleresultSheetType2Lable = new JLabel("Размер простыни: "+
                 SheetData[0][1]+" x "+SheetData[1][1]+" Количество: "+SheetData[2][1]
                 +" шт. Необходимо = "
-                +f.format(resultSheetType2/100.00)+" метров");
+                +f.format(fresultSheetType2)+" метров");
         titleresultSheetType2Lable.setVisible(false);
 
         JLabel exeptionSheetType2Lable = new JLabel("Простыни "+
@@ -446,10 +506,15 @@ public class ResultPage extends JFrame {
         int resultSheetType3 = SheetType3.CalculationSheet(SheetData[0][2], SheetData[1][2],
                 SheetData[2][2], SheetData[3][2], WidthCloth, HandSelectionSheet[2]);
 
+        float fresultSheetType3 = resultSheetType3/100.00f;
+        if (fresultSheetType3 < 0.01f && SheetData[2][2] != 0){
+            fresultSheetType3 = 0.01f;
+        }
+
         JLabel titleresultSheetType3Lable = new JLabel("Размер простыни: "+
                 SheetData[0][2]+" x "+SheetData[1][2]+" Количество: "+SheetData[2][2]
                 +" шт. Необходимо = "
-                +f.format(resultSheetType3/100.00)+" метров");
+                +f.format(fresultSheetType3)+" метров");
         titleresultSheetType3Lable.setVisible(false);
 
         JLabel exeptionSheetType3Lable = new JLabel("Простыни "+
@@ -478,10 +543,15 @@ public class ResultPage extends JFrame {
         int resultSheetType4 = SheetType4.CalculationSheet(SheetData[0][3], SheetData[1][3],
                 SheetData[2][3], SheetData[3][3], WidthCloth, HandSelectionSheet[3]);
 
+        float fresultSheetType4 = resultSheetType4/100.00f;
+        if (fresultSheetType4 < 0.01f && SheetData[2][3] != 0){
+            fresultSheetType4 = 0.01f;
+        }
+
         JLabel titleresultSheetType4Lable = new JLabel("Размер простыни: "+
                 SheetData[0][3]+" x "+SheetData[1][3]+" Количество: "+SheetData[2][3]
                 +" шт. Необходимо = "
-                +f.format(resultSheetType4/100.00)+" метров");
+                +f.format(fresultSheetType4)+" метров");
         titleresultSheetType4Lable.setVisible(false);
 
         JLabel exeptionSheetType4Lable = new JLabel("Простыни "+
@@ -510,10 +580,15 @@ public class ResultPage extends JFrame {
         int resultSheetType5 = SheetType5.CalculationSheet(SheetData[0][4], SheetData[1][4],
                 SheetData[2][4], SheetData[3][4], WidthCloth, HandSelectionSheet[4]);
 
+        float fresultSheetType5 = resultSheetType5/100.00f;
+        if (fresultSheetType5 < 0.01f && SheetData[2][4] != 0){
+            fresultSheetType5 = 0.01f;
+        }
+
         JLabel titleresultSheetType5Lable = new JLabel("Размер простыни: "+
                 SheetData[0][4]+" x "+SheetData[1][4]+" Количество: "+SheetData[2][4]
                 +" шт. Необходимо = "
-                +f.format(resultSheetType5/100.00)+" метров");
+                +f.format(fresultSheetType5)+" метров");
         titleresultSheetType5Lable.setVisible(false);
 
         JLabel exeptionSheetType5Lable = new JLabel("Простыни "+
